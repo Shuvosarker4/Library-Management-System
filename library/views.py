0792b0c6f6b9b0e489c20b1aa146fcdbf05ffa29
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-03-16 14:20:40 +0600

    authetication system edited

--- library/views.py
+++ library/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from library.models import Book,Author,BorrowRecord,Member
 from library.serializers import BookSerializer,AuthorSerializer,BorrowRecordSerializer,MemberSerializer
+from api.permissions import IsAdminOrReadOnly
 
 # Create your views here.
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 class BookViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BorrowRecordViewSet(viewsets.ModelViewSet):
     queryset = BorrowRecord.objects.all()
     serializer_class = BorrowRecordSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
+    permission_classes = [IsAdminOrReadOnly]
