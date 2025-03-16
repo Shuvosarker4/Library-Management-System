b7012489d3e88f6d9f25779f4bc8f29f91d105b3
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-03-16 14:29:36 +0600

    swagger and redoc implemented

--- library/views.py
+++ library/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from library.models import Book,Author,BorrowRecord,Member
 from library.serializers import BookSerializer,AuthorSerializer,BorrowRecordSerializer,MemberSerializer
 from api.permissions import IsAdminOrReadOnly
+from drf_yasg.utils import swagger_auto_schema
 
 # Create your views here.
 class AuthorViewSet(viewsets.ModelViewSet):
@@ -10,10 +11,29 @@ class AuthorViewSet(viewsets.ModelViewSet):
     serializer_class = AuthorSerializer
     permission_classes = [IsAdminOrReadOnly]
 
+    @swagger_auto_schema(
+        operation_summary='Retrive a list of Author'
+    )
+    def list(self, request, *args, **kwargs):
+        """Retrive all the Author"""
+        return super().list(request, *args, **kwargs)
+
+
 class BookViewSet(viewsets.ModelViewSet):
+    """
+        API endpoint for managing books in the library
+        - Allows authenticated admin to create, update, and delete books
+    """
     permission_classes = [IsAdminOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    @swagger_auto_schema(
+        operation_summary='Retrive a list of Books'
+    )
+    def list(self, request, *args, **kwargs):
+        """Retrive all the books"""
+        return super().list(request, *args, **kwargs)
 
 class BorrowRecordViewSet(viewsets.ModelViewSet):
     queryset = BorrowRecord.objects.all()
