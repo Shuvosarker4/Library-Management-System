c09945d0546df66be934a6d878e5b97c4ca0d68e
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-03-16 13:04:44 +0600

    models,serializer,viewset and route created

--- library/views.py
+++ library/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from library.models import Book,Author,BorrowRecord,Member
+from library.serializers import BookSerializer,AuthorSerializer,BorrowRecordSerializer,MemberSerializer
 
 # Create your views here.
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BorrowRecordViewSet(viewsets.ModelViewSet):
+    queryset = BorrowRecord.objects.all()
+    serializer_class = BorrowRecordSerializer
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
